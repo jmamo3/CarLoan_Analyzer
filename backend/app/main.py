c01a7e30764481755217fa604164f3a0534184cf
jmamo3 <joshuamamo05@gmail.com>
2026-05-19 23:07:19 -0700

    added description for fastAPI app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.routers import loan
 
-app = FastAPI()
+app = FastAPI(
+    title="CarLoan Analyzer API",
+    description="True cost of car ownership calculator",
+    version="0.1.0"
+)
 
 app.include_router(loan.router, prefix = "/api/loan", tags = ["loan"])
